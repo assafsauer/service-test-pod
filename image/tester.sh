7a158fc5e6c942bb2d7bfa2635edba3b31a01d03
assafsauer <as.sauer@gmail.com>
2023-03-09 22:59:01 +0100

    Update tester.sh

--- image/tester.sh
+++ image/tester.sh
@@ -1,46 +1,30 @@
 #!/bin/bash
 
-# Define the namespace and labels for each service
-NAMESPACE="my-namespace"
-MONGODB_LABELS="app=mongodb"
-RABBITMQ_LABELS="app=rabbitmq"
-NGINX_LABELS="app=nginx"
-SPRING_LABELS="app=spring"
-NODEJS_LABELS="app=nodejs"
+# Clear variables
+MONGO_POD=""
+RABBITMQ_POD=""
+POSTGRES_POD=""
+NODEJS_POD=""
 
-# Define the test command and report path for each service
-MONGODB_TEST_CMD="mongo --host my-mongodb-service --eval 'printjson(db.stats())'"
-MONGODB_REPORT_PATH="/reports/mongodb-report.txt"
-RABBITMQ_TEST_CMD="python3 rabbitmq-perf-test-2.11.0/bin/runjava com.rabbitmq.perf.PerfTest --uri amqp://my-rabbitmq-service --queue q1 --producers 2 --consumers 4 --time 30 --vsize 1000"
-RABBITMQ_REPORT_PATH="/reports/rabbitmq-report.txt"
-NGINX_TEST_CMD="ab -n 10000 -c 100 -H 'Accept-Encoding: gzip, deflate' http://my-nginx-service/"
-NGINX_REPORT_PATH="/reports/nginx-report.txt"
-SPRING_TEST_CMD="locust -f my-spring-test.py --host=http://my-spring-service:8080 --csv /reports/spring-report"
-SPRING_REPORT_PATH="/reports/spring-report.csv"
-NODEJS_TEST_CMD="loadtest http://my-nodejs-service:8080 --rps 100 --duration 30s --timeout 5s --concurrency 10"
-NODEJS_REPORT_PATH="/reports/nodejs-report.txt"
+# Find PODs running MongoDB, RabbitMQ, PostgreSQL and Node.js services
+MONGO_POD=$(kubectl get pods -l app=mongodb -o=jsonpath='{.items[0].metadata.name}')
+RABBITMQ_POD=$(kubectl get pods -l app=rabbitmq -o=jsonpath='{.items[0].metadata.name}')
+POSTGRES_POD=$(kubectl get pods -l app=postgres -o=jsonpath='{.items[0].metadata.name}')
+NODEJS_POD=$(kubectl get pods -l app=nodejs -o=jsonpath='{.items[0].metadata.name}')
 
-# Run the test and save the report for each service
-kubectl get pods -n $NAMESPACE -l $MONGODB_LABELS -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | while read pod; do
-  kubectl exec -n $NAMESPACE $pod -- /bin/bash -c "$MONGODB_TEST_CMD" > "$MONGODB_REPORT_PATH"
-done
+# Test MongoDB with YCSB
+echo "Testing MongoDB with YCSB..."
+kubectl exec $MONGO_POD -- bash -c "cd /ycsb-0.18.0/ && bin/ycsb load mongodb -s -P workloads/workloada"
 
-kubectl get pods -n $NAMESPACE -l $RABBITMQ_LABELS -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | while read pod; do
-  kubectl exec -n $NAMESPACE $pod -- /bin/bash -c "$RABBITMQ_TEST_CMD" > "$RABBITMQ_REPORT_PATH"
-done
+# Test RabbitMQ with rabbitmq-perf-test
+echo "Testing RabbitMQ with rabbitmq-perf-test..."
+kubectl exec $RABBITMQ_POD -- bash -c "cd /rabbitmq-perf-test-2.11.0/ && ./bin/runjava com.rabbitmq.perf.PerfTest --exchange perfTestEx --queue perfTestQ --flag persistent --size 1024 --count 10000"
 
-kubectl get pods -n $NAMESPACE -l $NGINX_LABELS -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | while read pod; do
-  kubectl exec -n $NAMESPACE $pod -- /bin/bash -c "$NGINX_TEST_CMD" > "$NGINX_REPORT_PATH"
-done
+# Test PostgreSQL with pgbench
+echo "Testing PostgreSQL with pgbench..."
+kubectl exec $POSTGRES_POD -- bash -c "pgbench -i -U postgres -d postgres"
+kubectl exec $POSTGRES_POD -- bash -c "pgbench -c 10 -T 60 -U postgres -d postgres"
 
-kubectl get pods -n $NAMESPACE -l $SPRING_LABELS -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | while read pod; do
-  kubectl exec -n $NAMESPACE $pod -- /bin/bash -c "$SPRING_TEST_CMD"
-done
-
-kubectl get pods -n $NAMESPACE -l $NODEJS_LABELS -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | while read pod; do
-  kubectl exec -n $NAMESPACE $pod -- /bin/bash -c "$NODEJS_TEST_CMD" > "$NODEJS_REPORT_PATH"
-done
-
-# Generate a report for each service
-mongo --quiet <(echo "db.stats()") > "$MONGODB_REPORT_PATH.html"
-python3 rabbitmq-perf-test-2.11
+# Test Node.js with Artillery
+echo "Testing Node.js with Artillery..."
+kubectl exec $NODEJS_POD -- bash -c "cd /app && artillery quick --duration 60 --rate 10 http://localhost:3000"
