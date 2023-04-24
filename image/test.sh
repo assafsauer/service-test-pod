a554c9ec05c604cec8d9fb390e68484fbc45cf7f
assafsauer <as.sauer@gmail.com>
2023-04-24 13:45:34 +0200

    Update test.sh

--- image/test.sh
+++ image/test.sh
@@ -1,61 +1,45 @@
 #!/bin/bash
 
-# Check for service arguments
-if [[ -z $1 ]]; then
-  echo "Usage: $0 [service1] [service2] ..."
-  exit 1
-fi
-
-# Function to check if a service exists
-function service_exists() {
-  local service=$1
-  local svc_exists=$(kubectl get svc --namespace=default | grep -w "${service}" | wc -l)
-  
-  if [ $svc_exists -eq 1 ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
 # Function to test a specific service with JMeter
 function test_service() {
   local service=$1
   local endpoint_url="${service}.default.svc.cluster.local"
 
-  if service_exists $service; then
-    case "$service" in
-      "mongodb")
-        echo "Testing MongoDB with JMeter..."
-        jmeter -n -t /jmeter-plans/mongodb.jmx -Jhost=$endpoint_url
-        ;;
-      "postgres")
-        echo "Testing PostgreSQL with JMeter..."
-        jmeter -n -t /jmeter-plans/postgres.jmx -Jhost=$endpoint_url
-        ;;
-      "rabbitmq")
-        echo "Testing RabbitMQ with JMeter..."
-        jmeter -n -t /jmeter-plans/rabbitmq.jmx -Jhost=$endpoint_url
-        ;;
-      "nodejs")
-        echo "Testing Node.js with JMeter..."
-        jmeter -n -t /jmeter-plans/nodejs.jmx -Jhost=$endpoint_url
-        ;;
-      "nginx")
-        echo "Testing Nginx with JMeter..."
-        jmeter -n -t /jmeter-plans/nginx.jmx -Jhost=$endpoint_url
-        ;;
-      *)
-        echo "Invalid service specified: $service"
-        ;;
-    esac
-  else
-    echo "Service $service not found, skipping the test."
-  fi
+  case "$service" in
+    "mongodb")
+      echo "Testing MongoDB with JMeter..."
+      jmeter -n -t /jmeter-plans/mongodb.jmx -Jhost=$endpoint_url
+      ;;
+    "postgres")
+      echo "Testing PostgreSQL with JMeter..."
+      jmeter -n -t /jmeter-plans/postgres.jmx -Jhost=$endpoint_url
+      ;;
+    "rabbitmq")
+      echo "Testing RabbitMQ with JMeter..."
+      jmeter -n -t /jmeter-plans/rabbitmq.jmx -Jhost=$endpoint_url
+      ;;
+    "nodejs")
+      echo "Testing Node.js with JMeter..."
+      jmeter -n -t /jmeter-plans/nodejs.jmx -Jhost=$endpoint_url
+      ;;
+    "nginx")
+      echo "Testing Nginx with JMeter..."
+      jmeter -n -t /jmeter-plans/nginx.jmx -Jhost=$endpoint_url
+      ;;
+    *)
+      echo "Invalid service specified: $service"
+      ;;
+  esac
 }
 
-# Loop through the provided service arguments and run tests for each of them
-for service in "$@"
+# Get the list of services
+services=$(kubectl get services --no-headers -o custom-columns=":metadata.name")
+
+# Loop through the discovered services and run tests for each of them
+for service in $services
 do
   test_service $service
 done
+
+# Sleep for a specified amount of time (e.g., 1 hour) before exiting
+sleep 1h
